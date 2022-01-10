79fb2b92cd248bec4d2fdad73a8a7f8bb39850db
ElvinsYan <yanxiangyang94@gmail.com>
2022-01-10 11:12:06 +0800

    添加 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      你所访问的页面不存在，请检查网址
+    </div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
