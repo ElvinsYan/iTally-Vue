2feb91be19d6d358be96bcc1262b8f775982f613
ElvinsYan <yanxiangyang94@gmail.com>
2022-01-10 13:14:08 +0800

    无法正确引入svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+      记账
+    </router-link>
     |
     <router-link to="/labels">标签</router-link>
     |
@@ -9,6 +14,16 @@
 </template>
 
 <script lang="ts">
+
+
+// eslint-disable-next-line no-undef
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('..assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+
 export default {
   name: 'Nav'
 };
