cd068dd31759bd3e0fa897b28e22e69ba029b15b
ElvinsYan <yanxiangyang94@gmail.com>
2022-01-11 22:46:07 +0800

    完成主要 HTML 和 CSS

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -29,9 +29,9 @@ export default {
 @import "~@/assets/style/helper.scss";
 
 nav {
+  @extend %outerShadow;
   display: flex;
   flex-direction: row;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   font-size: 12px;
 
   > .item {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,31 +1,8 @@
 <template>
-  <Layout>
-    <div class="tags">
-      <ul class="current">
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
-      </ul>
-      <div class="new-tag">
-        <button>新增标签</button>
-      </div>
-    </div>
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text">
-      </label>
-    </div>
-    <div>
-      <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
-      </ul>
-    </div>
+  <Layout class-prefix="layout">
     <div class="number-pad">
       <div class="output">100</div>
-      <div class="bottoms">
+      <div class="buttons">
         <button>1</button>
         <button>2</button>
         <button>3</button>
@@ -37,10 +14,33 @@
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button>OK</button>
-        <button>0</button>
+        <button class="ok">OK</button>
+        <button class="zero">0</button>
         <button>.</button>
       </div>
+    </div>
+    <div>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里输入备注">
+      </label>
+    </div>
+    <div class="tags">
+      <div class="new-tag">
+        <button>新增标签</button>
+      </div>
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
     </div>
   </Layout>
 </template>
@@ -51,6 +51,155 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
 
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
+.tags {
+  flex-grow: 1;
+  font-size: 14px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      background: #D9D9D9;
+      height: 24px;
+      line-height: 24px;
+      border-radius: 12px;
+      padding: 0 18px;
+      margin-right: 24px;
+      margin-top: 8px;
+    }
+  }
+
+  > .new-tag {
+    padding-top: 16px;
+
+    > button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+
+.notes {
+  background-color: #F5F5F5;
+  font-size: 14px;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+
+  > .name {
+    padding-right: 16px;
+  }
+
+  > input {
+    height: 72px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+  }
+}
+
+.types {
+  background: #C4C4C4;
+  display: flex;
+  font-size: 24px;
+
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected {
+      &::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333;
+      }
+    }
+  }
+}
+
+.number-pad {
+  > .output {
+    @extend %innerShadow;
+    font-size: 36px;
+    font-family: Consolas, monospace;
+    padding: 9px 16px;
+    text-align: right;
+  }
+
+  > .buttons {
+    @extend %clearFix;
+
+    > button {
+      width: 25%;
+      height: 64px;
+      float: left;
+      background: transparent;
+      border: none;
+
+      &.ok {
+        height: 64px*2;
+        float: right;
+      }
+
+      &.zero {
+        width: 25%*2;
+      }
+
+      $bg: #F2F2F2;
+
+      &:nth-child(1) {
+        background: $bg;
+      }
+
+      &:nth-child(2), &:nth-child(5) {
+        background: darken($bg, 4%);
+      }
+
+      &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+        background: darken($bg, 4%*2);
+      }
+
+      &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+        background: darken($bg, 4%*3);
+      }
+
+      &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+        background: darken($bg, 4%*4);
+      }
+
+      &:nth-child(14) {
+        background: darken($bg, 4%*5);
+      }
+
+      &:nth-child(12) {
+        background: darken($bg, 4%*6);
+      }
+    }
+  }
+}
 </style>
