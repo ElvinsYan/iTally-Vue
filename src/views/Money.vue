f7aa1a2dcbc2d5c9667f5f1e9c3f0b9bf2ad9fb4
ElvinsYan <yanxiangyang94@gmail.com>
2022-01-18 11:40:29 +0800

    封装出一个 model

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,26 +15,18 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
+import model from '@/model';
 
-const recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]');
-
-
-type Record = {
-  tags: string[],
-  notes: string,
-  type: string,
-  amount: number,
-  createdAt?: Date
-}
+const recordList = model.fetch();
 
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
-  recordList: Record[] = recordList;
+  recordList: RecordItem[] = recordList;
 
-  record: Record = {
+  record: RecordItem = {
     tags: [],
     notes: '',
     type: '-',
@@ -51,14 +43,14 @@ export default class Money extends Vue {
   }
 
   saveRecord(): void {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = model.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
 
   @Watch('recordList')
   onRecordListChanged(): void {
-    localStorage.setItem('record', JSON.stringify(this.recordList));
+    model.save(this.recordList);
   }
 }
 
