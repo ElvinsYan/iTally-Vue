144c69c7dda205b907a84eb5e0463ef44196e2f0
ElvinsYan <yanxiangyang94@gmail.com>
2022-01-19 17:38:14 +0800

    添加标签可跳转至编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,13 @@
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link class="tag"
+                   v-for="tag in tags" :key="tag.id"
+                   :to="`/labels/edit/${tag.id}`">
         <span>{{ tag.name }}</span>
         <Icon name="right"/>
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -42,7 +44,7 @@ export default class Labels extends Vue {
   background: #FFFFFF;
   padding-left: 16px;
 
-  > li {
+  > .tag {
     min-height: 44px;
     border-bottom: 1px solid #BCBBC1;
     display: flex;
