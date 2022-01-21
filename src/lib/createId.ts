8d0c60273c1df409f6d322f4bcd8bc5c28115f5e
ElvinsYan <yanxiangyang94@gmail.com>
2022-01-21 15:17:57 +0800

    fix bugs

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -3,6 +3,7 @@ let id: number = parseInt(window.localStorage.getItem('_idMax') || '0') || 0;
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function CreateId() {
   id++;
+  window.localStorage.setItem('_idMax', id.toString());
   return id;
 }
 
