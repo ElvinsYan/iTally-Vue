536e4e8cffa47e52c1912845f9d0d6e4a06b9c1c
ElvinsYan <yanxiangyang94@gmail.com>
2022-01-21 16:10:33 +0800

    将 create 封装到 recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -49,14 +49,12 @@ export default class Money extends Vue {
   }
 
   saveRecord(): void {
-    const record2: RecordItem = recordListModel.clone(this.record);
-    record2.createdAt = new Date();
-    this.recordList.push(record2);
+    recordListModel.create(this.record);
   }
 
   @Watch('recordList')
   onRecordListChanged(): void {
-    recordListModel.save(this.recordList);
+    recordListModel.save();
   }
 }
 
