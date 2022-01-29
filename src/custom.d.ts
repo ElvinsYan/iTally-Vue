88b244e42ca033a4212bb434ea3bcee3e07694dd
ElvinsYan <yanxiangyang94@gmail.com>
2022-01-29 20:16:21 +0800

    封装 TagList 的增删改查

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -20,6 +20,9 @@ type TagListModel = {
 }
 
 interface Window {
-  tagList: tag[];
+  tagList: Tag[];
+  findTag: (id: string) => Tag | undefined;
   createTag: (name: string) => void;
+  removeTag: (id: string) => boolean;
+  updateTag: (id: string, name: string) => 'success' | 'not_found' | 'duplicated';
 }
