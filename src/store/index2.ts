3628ee6704b8d6bebef7cfee5c0169a20ee312f2
ElvinsYan <yanxiangyang94@gmail.com>
2022-02-10 15:31:05 +0800

    模块化的 store，删除了 models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,30 +1,9 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-  //record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem): void => recordListModel.create(record),
-
-  //tag store
-  tagList: tagListModel.fetch(),
-  findTag(id: string): Tag | undefined {
-    return this.tagList.filter(t => t.id === id)[0];
-  },
-  createTag: (name: string): void => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('标签名重复，请检查');
-    } else if (message === 'success') {
-      window.alert('标签添加成功');
-    }
-  },
-  removeTag: (id: string): boolean => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string): 'success' | 'not_found' | 'duplicated' => {
-    return tagListModel.update(id, name);
-  },
+  ...recordStore,
+  ...tagStore
 };
 
 export default store;
