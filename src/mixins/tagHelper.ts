c0803f352b3730fc69e7602cfc0bea580c4852d8
ElvinsYan <yanxiangyang94@gmail.com>
2022-03-07 23:27:46 +0800

    修复一进入页面就弹出 4 个提示的 bug

--- src/mixins/tagHelper.ts
+++ src/mixins/tagHelper.ts
@@ -1,11 +1,18 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
+const map: { [key: string]: string } = {
+  'tag name duplicated': '标签名重复'
+};
+
 @Component
 export class TagHelper extends Vue {
   createTag(): void {
     const name = window.prompt('请输入标签');
     if (!name) {return;}
     this.$store.commit('createTag', name);
+    if (this.$store.state.createTagError) {
+      window.alert(map['tag name duplicated'] || '未知错误');
+    }
   }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -9,6 +9,7 @@ const store = new Vuex.Store({
   state: {
     recordList: [],
     createRecordError: null,
+    createTagError: null,
     tagList: [],
     currentTag: undefined
   } as RootState,
@@ -38,14 +39,15 @@ const store = new Vuex.Store({
       }
     },
     createTag(state, name: string) {
+      state.createTagError = null;
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
-        window.alert('标签名重复，请检查');
+        state.createTagError = new Error('tag name duplicated');
+        return;
       }
       const id = createId().toString();
       state.tagList.push({id, name});
       store.commit('saveTags');
-      window.alert('标签添加成功');
     },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
